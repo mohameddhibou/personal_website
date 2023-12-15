8becb649da7bdf6496b59fd684b04687976ad56a
DHIBOU Mohamed <95587035+mohameddhibou@users.noreply.github.com>
2023-12-15 10:44:27 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,295 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Dhibou Mohamed &copy</title>
+	<meta charset="utf-8" />
+ 	<meta name="viewport" content="width-device-width, initial-scale=0.7">
+	<meta name="keywords" content="Moohamed,Dhibou,Mohamed Dhibou,Website,Rabat,Morocco" />
+	<meta name="author" content="Dhibou Mohamed" />
+	<meta name="description" content="Welcome to my firt and omly website. " />
+	<link rel="stylesheet" href="style.css">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="shortcut icon" type="image/png" href="imgs/logoT.png">
+	<script src="main.js" defer></script>
+</head>
+<body>
+
+<!---------------------------------------->
+<section id="nav-bar">
+		<nav class="navbar navbar-expand-lg navbar-light">
+  <a class="navbar-brand" href="#"><img src="imgs/logoT.png"></a>
+  <a class="momo" href="#"><img src="imgs/logo.png"></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon">
+    </span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="#top">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#about-me">About me</a>
+      </li>
+<!------------------------------------------DROPDOWN--------------------------------------------------->
+	<li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Career
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#studies">Studies</a>
+          <a class="dropdown-item" href="#hobbies">Hobbies</a>
+      </li>
+<!------------------------------------------------------------------------------------------------------>
+      <li class="nav-item">
+        <a class="nav-link" href="#footer">Contact</a>
+      </li>
+    </ul>
+  </div>
+</nav>	
+</section>
+<!------------------banner section--------------->
+<section id="banner">
+	<div class="container">
+	<div class="row">
+	<div class="col-md-6">
+		<p class="promo-title">Mohamed Dhibou </p>
+		<div class="tfou"><p>Welcome to my website.</p></div>
+	</div>
+		<div class="col-md-6 text-center">
+		<!-----------------------------------------SLIDE------------------->
+		<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="imgs/hello.png" class="d-block w-100">
+    </div>
+    <div class="carousel-item">
+      <img src="imgs/hello1.png" class="d-block w-100">
+    </div>
+    <div class="carousel-item">
+      <img src="imgs/hello5.png" class="d-block w-100">
+    </div>
+    <div class="carousel-item">
+      <img src="imgs/hello2.png" class="d-block w-100">
+    </div>
+    <div class="carousel-item">
+      <img src="imgs/hello3.png" class="d-block w-100">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+		<!-----------------------------------------end slide--------------->
+		</div>
+	</div>
+	</div>
+	<img src="imgs/wave.png" class="bottom-img">
+</section>
+
+
+<!------------------Service section------------------->
+<section id="about-me">
+	<div class="container">
+		<h1 class="title text-center">About me:</h1>
+	<div class="row">
+	<div class="col-md-5 about-me">
+		<ul>
+			<li>Mohamed Dhibou i am 22 years old from Rabat, Morocco.</li>
+			<li>Bachelor degree in Software Development.</li>
+			<li>Currently studying at UPEC (Université Paris-Est Créteil).
+			</li>
+			<li>I am preparing a Master in
+				Biometrics and Intelligent Vision.
+			</li>
+			<li>I am a sociable person and adaptable who is interested in
+			new experiences.</li>
+			<li>My most
+			strong features are perseverance, curiosity and
+			producing quality work in
+			a team environment.</li>
+		</ul>	
+	</div>
+	<div class="col-md-6">
+		<img src="imgs/AboutMe.png" class="img-fluid">
+	</div>
+	</div>
+	</div>
+</section>
+<!-----------------------Studies----------------->
+<section id="studies">
+	<div class="container">
+		<h1 class="title1 text-center"> Studies:</h1>
+	<div class="row">
+	<div class="col-md-6 studies">
+	<table id="customers">
+  <tr>
+    <th>Level</th>
+    <th>School's name</th>
+    <th>School's location</th>
+    <th>School year</th>
+  </tr>
+  <tr>
+    <td class="td1">Elementry School</td>
+    <td class="td2">G.S.P AL MOUBADARA</td>
+    <td class="td2"><a href="https://www.google.com/maps/place/Groupe+Scolaire+priv%C3%A9+Almoubadara/@33.9675237,-6.8958776,17z/data=!3m1!4b1!4m5!3m4!1s0xda76d3131768707:0x64cb09d1669f134e!8m2!3d33.9675237!4d-6.8936889?hl=fr" target="_blank">Amal 6,N°5-Hay El Fath, Rabat</a></td>
+    <td class="td2">(2006-2013)</td>
+  </tr>
+  <tr>
+    <td class="td1">Middle School</td>
+    <td class="td2">G.S.P AL MOUBADARA</td>
+    <td class="td2"><a href="https://www.google.com/maps/place/Groupe+Scolaire+priv%C3%A9+Almoubadara/@33.9675237,-6.8958776,17z/data=!3m1!4b1!4m5!3m4!1s0xda76d3131768707:0x64cb09d1669f134e!8m2!3d33.9675237!4d-6.8936889?hl=fr" target="_blank">Amal 6,N°5-Hay El Fath, Rabat</a></td>
+    <td class="td2">(2013-2016)</td>
+  </tr>
+  <tr>
+    <td class="td1">High School</td>
+    <td class="td2">ABIDAR EL GHIFARI</td>
+    <td class="td2"><a href="https://www.google.com/maps/place/Lyc%C3%A9e+Abi+Darr+Al+Ghaffari/@33.9659072,-6.9013029,17z/data=!3m1!4b1!4m5!3m4!1s0xda76d36068f7a0b:0x54aa49ffa4647ea5!8m2!3d33.9659072!4d-6.8991142?hl=fr" target="_blank">Lycée Abi Darr Al Ghaffari, Rabat</a></td>
+    <td class="td2">(2016-2019)</td>
+  </tr>
+  <tr>
+    <td class="td1">DTS Degree</td>
+    <td class="td2">OFPPT ISTA NTIC</td>
+    <td class="td2"><a href="https://www.google.com/maps/place/ISTA+NTIC/@33.9614595,-6.8717953,17z/data=!3m1!4b1!4m5!3m4!1s0xda7132d2a3728ab:0x22a9ca17b33c74ae!8m2!3d33.9614595!4d-6.8696066?hl=fr" target="_blank">Rue Assendiane, Hay Riad, Rabat</a></td>
+    <td class="td2">(2019-2021)</td>
+  </tr>
+  <tr>
+    <td class="td1">Bachelor Degree</td>
+    <td class="td2">FST Settat</td>
+    <td class="td2"><a href="https://www.google.fr/maps/place/FST+:+Facult%C3%A9+des+Sciences+et+Techniques_Settat/@33.0309077,-7.6190799,17z/data=!3m1!4b1!4m6!3m5!1s0xda61aabf2a6a8b9:0xe6a579c28d993de9!8m2!3d33.0309077!4d-7.616505!16s%2Fg%2F1tjjqqs_?entry=ttu" target="_blank">BP : 577, Route de Casa, Settat, Maroc</a></td>
+    <td class="td2">(2021-2022)</td>
+  </tr>
+  <tr>
+    <td class="td1">Master Degree</td>
+    <td class="td2">UPEC</td>
+    <td class="td2"><a href="https://www.google.fr/maps/place/Universit%C3%A9+Paris-Est+Cr%C3%A9teil+(UPEC)/@48.788159,2.441076,17z/data=!3m1!4b1!4m6!3m5!1s0x47e5fa80a0ccbbf5:0x62a1cc11b303c8f!8m2!3d48.788159!4d2.4436509!16s%2Fm%2F027nknf?entry=ttu" target="_blank">61 Av. du Général de Gaulle, 94000 Créteil</a></td>
+    <td class="td2">(2022-2024)</td>
+  </tr>
+</table>
+	</div>
+	<div class="col-md-6">
+		<img src="imgs/studies2.png" class="studies-img">
+	</div>
+	</div>
+	</div>
+</section>
+
+<!----------------------------------------------->
+<!-----------------------Hobbies----------------->
+<section id="hobbies">
+	<div class="container">
+		<h1 class="title2 text-center"> Hobbies:</h1>
+	<div class="row">
+	<div class="col-md-4 hobbies">
+	<img src="imgs/sport.png" class="testimonials-img">
+		<h4>Sport:</h4>
+		<p>In general I am in love with Sports and it is one of my best and favourite hobbies since I was child, my main Sport that I am still practicing since 2014 is a Martial Art named Haidong Gumdo founded in south of Korea. As I said I started this nobel Martial Art in 2014 until now and I am black belted. <a href="https://fr.wikipedia.org/wiki/Haidong_gumdo" target="_blank">Click here</a> for more information about Haidong Gumdo, Also <a href="https://www.sabrecoreen-idf.fr/" target="_blank">click here</a> to visit my Dojang (school) website. Before 2014 I practiced different sports like Soccer, and two other Martial Arts which are Volam or Vietmanese Martial Art, this one was perfect but seems to be less famous than other arts, and <a href="https://fr.wikipedia.org/wiki/Hapkido" target="_blank">Hapkido</a>, a korean Martial Art same as Haidong Gumdo. Also I am interested in Basketball and I practice it a lot with my friends.</p>
+	</div>
+	<div class="col-md-4 hobbies">
+	<img src="imgs/programming.png" class="testimonials-img">
+		<h4>Programming:</h4>
+		<p>Actually Programming is a part of my academic studies but it became one of my hobbies I loved it despite I am still learning and I look forward to emprove my skills and learn more technologies, I started learning C language and JAVA8 during my DTS studies during my free time I practice Web programming (HTML5, CSS, JavaScript, PHP), and currently I am in the field of AI and Machine Learning as a Master's student. It is not only adding new skills but it is also a way to take a fresh look at life and boosts brain. It is true that programming skills do not come easy, but they will if we keep up the practice.</p>
+		<ul>
+			<li><a href="https://en.wikipedia.org/wiki/Computer_programming" target="_blank">What is programming?</a></li>
+			<li><a href="https://en.wikipedia.org/wiki/Programming_language" target="_blank">What is programming languages?</a></li>
+			<li><a href="https://fr.wikipedia.org/wiki/Programmation_web" target="_blank">What is web programming?</a></li>
+		</ul>
+	</div>	
+	<div class="col-md-4 hobbies">
+	<img src="imgs/draw.png" class="testimonials-img">
+		<h4>Drawing:</h4>
+		<p>Drawing is a great hobby for those who know what patience is. This means that more you are patient more you draw masterpieces. Though I am a beginner, but believe me if you can handle fishing the whole day and getting back home bare handed, you can handle drawing. I started drawing in 2015, I have to admit that I was an absolute beginner. I tought that I could never make it. I Started learning and practicing, I was trying my best to learn as much as I can. As the days go by I improved my skills and started having self confidence and believing in myself. My experience with drawing taught me to never give up and to always fight and struggle to achieve your goals and dreams.</p>
+		<ul>
+			<li><a href="https://drive.google.com/drive/folders/1YzFPeC9-mdyzBCn9Hj9QlXi7jVUvbdZS?usp=sharing" target="_blank">Click here</a> to see some of my drawings</li>
+		</ul>
+	</div>
+	</div>
+	</div>
+</section>
+<!---------------------------------------social media------->
+<section id="social-media">
+	<div class="container text-center">
+		<p class="title3">Find me on social media</p>
+		<div class="socials-icons">
+		<a href="https://mail.google.com/" target="_blank"><img src="imgs/gmail.png" alt="Gmail"></a>
+		<a href="https://www.facebook.com/mohameddhibou4" target="_blank"><img src="imgs/facebook.png " alt="Facebook"></a>
+		<a href="https://www.youtube.com/channel/UCKU3dAFFavM1yrQoZq1xR9g?view_as=subscriber" target="_blank"><img src="imgs/youtube.png" alt="Youtube"></a>
+		<a href="https://www.instagram.com/mohameddhibou/" target="_blank"><img src="imgs/instagram.png" alt="Instagram"></a>
+		<a href="https://www.linkedin.com/in/mohamed-dhibou-4623341a0/" target="_blank"><img src="imgs/linkedin.png" alt="Linkedin"></a>
+	</div>
+	</div>
+</section>
+
+
+
+<section id="social-media">
+	<div class="container text-center">
+		<p class="title4">Stickers made by Bitmoji</p>
+		<div class="socials-icons">
+		<a href="https://play.google.com/store/apps/details?id=com.bitstrips.imoji&hl=fr" target="_blank"><img src="imgs/playstore.png " alt="Play Store"></a>
+		<a href="https://apps.apple.com/us/app/bitmoji/id868077558" target="_blank"><img src="imgs/appstore.png" alt="App Store"></a>
+	</div>
+</div>
+</section>
+<!-------------------------footer section------------>
+<section id="footer">
+	<img src="imgs/wave1.png" class="footer-img">	
+	<div class="container">
+	<div class="row">
+	<div class="col-md-4 footer-box" >
+	<img src="imgs/MohamedDhibou.JPG">
+	<p class="TTT">  Mohamed  Dhibou</p>
+	</div>	
+	<div class="col-md-4 footer-box">
+		<p><b>CONTACT</b></p>
+		<p><i class="fa fa-map-marker"></i>Paris, France.</p>
+		<p><i class="fa fa-phone"></i>+33 6 65 37  71 57</p>
+		<p class="fre"><i class="fa fa-envelope-o"></i>mohamed.dhibou1234@gmail.com</p>
+	</div>
+		<div class="col-md-4 footer-box">
+			<p><b>TEXT ME</b></p>
+			<!-------------php---------------------->
+			<form method="POST">
+				<input type="name" id="name" name="name" class="form-control" placeholder="Enter your name." required="">
+				<input type="email" id="email" name="email" class="form-control" placeholder="Enter your Email." required="">
+				<textarea name="message" id="message" value="message" class="form-control" placeholder="Type your message" required=""></textarea>
+				<button type="submit" name="submit" id="send" class="btn btn-primary">Send</button>
+			</form>
+			<?php
+				$name = $_POST['name'];
+				$email = $_POST['email'];
+				$message = $_POST['message'];
+				// Database connection
+				$conn = new mysqli('','','','');
+				if(isset($_POST['submit'])){
+					$stmt = $conn->prepare("insert into table(name, email, message) values(?, ?, ?)");
+					$stmt->bind_param("sss", $name, $email, $message);
+					$execval = $stmt->execute();
+					echo "<script type='text/javascript'>
+							window.alert('Thanks $name for your message!');
+						</script>";
+					$stmt->close();
+					$conn->close();
+				}
+			?>
+		</div>
+	</div>	
+		<hr>
+		<p class="copyright">Website created by Mohamed Dhibou &copy 2023</p>
+	</div>
+</section>
+<!-------------------smooth scroll---------->
+	<script src="smooth-scroll.js">
+	var scroll = new SmoothScroll('a[href*="#"]');
+	</script>
+</body>
+</html>
